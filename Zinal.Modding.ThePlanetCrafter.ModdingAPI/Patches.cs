2addf10282becebaa705950c30e86fa4130fe4fb
Zinal <Zinal001@users.noreply.github.com>
2022-03-29 22:53:23 +0200

    Add project files.

---
+++ Zinal.Modding.ThePlanetCrafter.ModdingAPI/Patches.cs
@@ -0,0 +1,165 @@
+﻿using HarmonyLib;
+using MijuTools;
+using SpaceCraft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zinal.Modding.ThePlanetCrafter.ModdingAPI
+{
+    internal static class Patches
+    {
+        [HarmonyPatch(typeof(Localization), "LoadLocalization")]
+        [HarmonyPostfix()]
+        public static void Localization_LoadLocalization_Postfix(bool ___hasLoadedSuccesfully)
+        {
+            if (!LocalizationEx.LocalizationLoaded && ___hasLoadedSuccesfully)
+            {
+                LocalizationEx.LocalizationLoaded = true;
+                LocalizationEx.TriggerLoaded();
+            }
+
+            LocalizationEx.LocalizationLoaded = ___hasLoadedSuccesfully;
+        }
+
+
+        #region Events - GameState
+        [HarmonyPatch(typeof(SavedDataHandler), "LoadSavedData")]
+        [HarmonyPrefix()]
+        public static void SavedDataHandler_LoadSavedData_Prefix(SavedDataHandler __instance)
+        {
+            Events.GameState.Trigger_GameLoading(__instance, __instance.saveFileName);
+        }
+
+        [HarmonyPatch(typeof(SavedDataHandler), "LoadSavedData")]
+        [HarmonyPostfix()]
+        public static void SavedDataHandler_LoadSavedData_Postfix(SavedDataHandler __instance)
+        {
+            Events.GameState.Trigger_GameLoaded(__instance, __instance.saveFileName);
+        }
+
+        [HarmonyPatch(typeof(SavedDataHandler), "SaveWorldData")]
+        [HarmonyPrefix()]
+        public static void SavedDataHandler_SaveWorldData_Prefix(SavedDataHandler __instance, String _forceSaveFileName)
+        {
+            Events.GameState.Trigger_GameSaving(__instance, String.IsNullOrEmpty(_forceSaveFileName) ? __instance.saveFileName : _forceSaveFileName);
+        }
+
+        [HarmonyPatch(typeof(SavedDataHandler), "SaveWorldData")]
+        [HarmonyPostfix()]
+        public static void SavedDataHandler_SaveWorldData_Postfix(SavedDataHandler __instance, String _forceSaveFileName)
+        {
+            Events.GameState.Trigger_GameSaved(__instance, String.IsNullOrEmpty(_forceSaveFileName) ? __instance.saveFileName : _forceSaveFileName);
+        }
+        #endregion
+
+        #region Events - Weather
+        [HarmonyPatch(typeof(MeteoHandler), "QueueMeteoEvent")]
+        [HarmonyPrefix]
+        public static bool MeteoHandler_QueueMeteoEvent_Prefix(MeteoHandler __instance, MeteoEventData _meteoEvent)
+        {
+            if (Events.Weather.Trigger_WeatherQueued(__instance, _meteoEvent))
+                return false;
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(MeteoHandler), "LaunchSpecificMeteoEvent")]
+        [HarmonyPrefix]
+        public static bool MeteoHandler_LaunchSpecificMeteoEvent_Prefix(MeteoHandler __instance, MeteoEventData _meteoEvent)
+        {
+            if (Events.Weather.Trigger_WeatherChanged(__instance, _meteoEvent))
+                return false;
+
+            return true;
+        }
+
+        #endregion
+
+        #region Events - Machines
+
+        [HarmonyPatch(typeof(MachineGenerator), "GenerateAnObject")]
+        [HarmonyPrefix]
+        public static bool MachineGenerator_GenerateAnObject_Prefix(MachineGenerator __instance, Inventory ___inventory, WorldObject ___worldObject, bool ___hasEnergy)
+        {
+            WorldObject worldObject = WorldObjectsHandler.CreateNewWorldObject(GroupsHandler.GetGroupViaId(__instance.groupDatas[UnityEngine.Random.Range(0, __instance.groupDatas.Count)].id), 0);
+
+            if(Events.Machines.Trigger_ObjectGenerated(__instance, ___hasEnergy, ___inventory, worldObject))
+            {
+                WorldObjectsHandler.DestroyWorldObject(worldObject);
+                return false;
+            }
+
+            ___inventory.AddItem(worldObject);
+
+            return false;
+        }
+        #endregion
+
+        #region Events - Building
+
+        [HarmonyPatch(typeof(PlayerBuilder), "SetNewGhost")]
+        [HarmonyPrefix]
+        public static bool PlayerBuilder_SetNewGhost_Prefix(PlayerBuilder __instance, GroupConstructible groupConstructible)
+        {
+            if (Events.Building.Trigger_BuildModeActivated(__instance, groupConstructible))
+                return false;
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(PlayerBuilder), "InputOnCancelAction")]
+        [HarmonyPrefix]
+        public static bool PlayerBuilder_InputOnCancelAction_Prefix(PlayerBuilder __instance, ConstructibleGhost ___ghost, GroupConstructible ___ghostGroupConstructible)
+        {
+            if (Events.Building.Trigger_BuildModeDeactivated(__instance, ___ghost, ___ghostGroupConstructible, true))
+                return false;
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(PlayerBuilder), "InputOnAction")]
+        [HarmonyPrefix]
+        public static bool PlayerBuilder_InputOnAction_Prefix(PlayerBuilder __instance, float ___timeCreatedGhost, float ___timeCantBuildInterval, ref ConstructibleGhost ___ghost, GroupConstructible ___ghostGroupConstructible)
+        {
+            if (___ghost != null)
+            {
+                if (Events.Building.Trigger_BuildModeDeactivated(__instance, ___ghost, ___ghostGroupConstructible, false))
+                    return false;
+
+                if (UnityEngine.Time.time < ___timeCreatedGhost + ___timeCantBuildInterval && !Managers.GetManager<PlayModeHandler>().GetIsFreePlay())
+                    return false;
+
+                UnityEngine.GameObject gameObject = ___ghost.Place();
+
+                if (gameObject != null)
+                {
+                    UnityEngine.Quaternion ghostRotation = new UnityEngine.Quaternion(___ghost.transform.rotation.x, ___ghost.transform.rotation.y, ___ghost.transform.rotation.z, ___ghost.transform.rotation.w);
+                    ___ghost = null;
+
+                    __instance.GetComponent<PlayerAudio>().PlayBuildGhost();
+                    __instance.GetComponent<PlayerAnimations>().AnimateConstruct(true);
+                    __instance.Invoke("StopAnimation", 0.5f);
+                    UnityEngine.Material constructionMaterial = Managers.GetManager<VisualsResourcesHandler>().GetConstructionMaterial();
+                    gameObject.AddComponent<GhostFx>().StartDisolveAnimation(-0.015f, 0.7f, constructionMaterial);
+                    Inventory inventory = __instance.GetComponent<PlayerBackpack>().GetInventory();
+
+                    if(inventory.ContainsItems(new List<Group>() { ___ghostGroupConstructible }))
+                        inventory.RemoveItems(new List<Group>() { ___ghostGroupConstructible }, true, true);
+                    else
+                        inventory.RemoveItems(___ghostGroupConstructible.GetRecipe().GetIngredientsGroupInRecipe(), true, true);
+
+                    Events.Building.Trigger_BuildingBuilt(__instance, ghostRotation, ___ghostGroupConstructible, gameObject);
+                }
+            }
+
+            return false;
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ Zinal.Modding.ThePlanetCrafter.OreExtractorFullWarning/Plugin.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx;
+using System.Linq;
+using Zinal.Modding.ThePlanetCrafter.ModdingAPI;
+
+namespace Zinal.Modding.ThePlanetCrafter.OreExtractorFullWarning
+{
+    [BepInPlugin("Zinal.Modding.ThePlanetCrafter.OreExtractorFullWarning", "Ore Extractor Full Warning", PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("Zinal.Modding.ThePlanetCrafter.ModdingAPI", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInProcess("Planet Crafter.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static readonly string[] MachineGenerator_Ignore = new string[] { "WaterBottle1", "TreeRoot" };
+        private static readonly string[] MachineGenerator_Ores = new string[] { "Super Alloy", "Osmium", "Sulfur", "Aluminium", "Iridium", "Uranium", "Iron" };
+
+        internal static BepInEx.Logging.ManualLogSource PluginLogger { get; private set; }
+
+        static Plugin()
+        {
+            HarmonyLib.Harmony.CreateAndPatchAll(typeof(Patches));
+        }
+
+        public Plugin()
+        {
+            PluginLogger = Logger;
+        }
+
+        private void Awake()
+        {
+            Logger.LogInfo("Plugin was loaded correctly");
+
+            LocalizationEx.OnLocalizationLoaded += LocalizationEx_OnLocalizationLoaded;
+            Events.Machines.ObjectGenerated += Machines_ObjectGenerated;
+        }
+
+        private void Machines_ObjectGenerated(object sender, Events.Machines.MachineGeneratorEventArgs e)
+        {
+            if(sender is SpaceCraft.MachineGenerator generator)
+            {
+                if(e.Inventory.GetInsideWorldObjects().Count == e.Inventory.GetSize() - 1)
+                {
+                    if (generator.groupDatas.Any(g => MachineGenerator_Ignore.Contains(g.id)))
+                        return;
+
+                    string oreName = "";
+
+                    foreach (string ore in MachineGenerator_Ores)
+                    {
+                        if (generator.groupDatas.Any(g => g.id == ore))
+                        {
+                            oreName = ore;
+                            break;
+                        }
+                    }
+
+                    if (string.IsNullOrEmpty(oreName))
+                        oreName = "Iron";
+
+                    string pos = $"{generator.transform.position.x}:{generator.transform.position.y}:{generator.transform.position.z}";
+                    Plugin.PluginLogger.LogDebug($"Ore extractor mining {oreName} at {pos} is full");
+                    MijuTools.Managers.GetManager<SpaceCraft.BaseHudHandler>().DisplayCursorText("ORE_EXTRACTOR_FULL", 10f, $"Mining {oreName} at {pos}");
+                }
+            }
+        }
+
+        private void LocalizationEx_OnLocalizationLoaded(object sender, System.EventArgs e)
+        {
+            Logger.LogDebug("LocalizationEx has been loaded!");
+            LocalizationEx.SetLocalizedText("ORE_EXTRACTOR_FULL", "Ore Extractor Full - ");
+        }
+    }
+}
