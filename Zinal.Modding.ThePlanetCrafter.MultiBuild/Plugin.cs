1ffb27848bed8546752fc2fcaa3783cd6ae4a1ff
Zinal <Zinal001@users.noreply.github.com>
2022-03-29 23:44:04 +0200

    MultiBuild - v1.0.1: - Added check to see if a player has the required ingredients to build multiple copies.

--- Zinal.Modding.ThePlanetCrafter.MultiBuild/Plugin.cs
+++ Zinal.Modding.ThePlanetCrafter.MultiBuild/Plugin.cs
@@ -1,8 +1,9 @@
 ﻿using BepInEx;
+using System.Collections.Generic;
 
 namespace Zinal.Modding.ThePlanetCrafter.MultiBuild
 {
-    [BepInPlugin("Zinal.Modding.ThePlanetCrafter.MultiBuild", "MultiBuild", "1.0.0")]
+    [BepInPlugin("Zinal.Modding.ThePlanetCrafter.MultiBuild", "MultiBuild", "1.0.1")]
     [BepInDependency("Zinal.Modding.ThePlanetCrafter.ModdingAPI", BepInDependency.DependencyFlags.HardDependency)]
     [BepInProcess("Planet Crafter.exe")]
     public class Plugin : BaseUnityPlugin
@@ -20,11 +21,15 @@ namespace Zinal.Modding.ThePlanetCrafter.MultiBuild
 
             if(UnityEngine.InputSystem.Keyboard.current[UnityEngine.InputSystem.Key.LeftShift].isPressed || UnityEngine.InputSystem.Keyboard.current[UnityEngine.InputSystem.Key.RightShift].isPressed)
             {
-                playerBuilder.SetNewGhost(e.GroupConstructible);
+                List<SpaceCraft.Group> ingredients = e.GroupConstructible.GetRecipe().GetIngredientsGroupInRecipe();
+                if(MijuTools.Managers.GetManager<SpaceCraft.PlayersManager>().GetActivePlayerController().GetPlayerBackpack().GetInventory().ContainsItems(ingredients) || MijuTools.Managers.GetManager<SpaceCraft.PlayModeHandler>().GetFreeCraft())
+                {
+                    playerBuilder.SetNewGhost(e.GroupConstructible);
 
-                SpaceCraft.ConstructibleGhost newGhost = (SpaceCraft.ConstructibleGhost)ghostField.GetValue(playerBuilder);
-                if (newGhost != null)
-                    newGhost.gameObject.transform.rotation = e.GhostRotation;
+                    SpaceCraft.ConstructibleGhost newGhost = (SpaceCraft.ConstructibleGhost)ghostField.GetValue(playerBuilder);
+                    if (newGhost != null)
+                        newGhost.gameObject.transform.rotation = e.GhostRotation;
+                }
             }
         }
     }
